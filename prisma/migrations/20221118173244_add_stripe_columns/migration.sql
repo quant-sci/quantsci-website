0e2ac7b034c52fd591f2cd2119cd8ff86a057ebb
Kleyt0n <kleyton.vsc@gmail.com>
2024-02-12 01:07:02 -0300

    update: build

--- prisma/migrations/20221118173244_add_stripe_columns/migration.sql
+++ prisma/migrations/20221118173244_add_stripe_columns/migration.sql
@@ -13,15 +13,3 @@ ALTER TABLE `posts` DROP FOREIGN KEY `posts_authorId_fkey`;
 
 -- DropForeignKey
 ALTER TABLE `sessions` DROP FOREIGN KEY `sessions_userId_fkey`;
-
--- AlterTable
-ALTER TABLE `users` ADD COLUMN `stripe_current_period_end` DATETIME(3) NULL,
-    ADD COLUMN `stripe_customer_id` VARCHAR(191) NULL,
-    ADD COLUMN `stripe_price_id` VARCHAR(191) NULL,
-    ADD COLUMN `stripe_subscription_id` VARCHAR(191) NULL;
-
--- CreateIndex
-CREATE UNIQUE INDEX `users_stripe_customer_id_key` ON `users`(`stripe_customer_id`);
-
--- CreateIndex
-CREATE UNIQUE INDEX `users_stripe_subscription_id_key` ON `users`(`stripe_subscription_id`);
